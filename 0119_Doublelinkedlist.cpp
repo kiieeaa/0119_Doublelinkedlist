60510d86a5a44987372de08fb4b2a19fd6b9499e
kiieeaa <kiyaadinda5@gmail.com>
2024-05-03 09:53:27 +0700

    step 2

--- 0119_Doublelinkedlist.cpp
+++ 0119_Doublelinkedlist.cpp
@@ -6,14 +6,21 @@ struct Node
 	Node *prev;
 };
 
-Node* START = NULL;
+Node *START = NULL;
 
 void addNode()
 	{
-		Node* newNode = new Node(); // step 1: create a new node cout << "\nEnter the roll number of the student: ";
-		coun << "\nEnter the roll number of the student:";
-		cin >> newNode->noMhs; // Assign value to the data field of the new node
+		Node *newNode = new Node(); // step 1: create a new node 
+		coun << "\nEnter the roll number of the student : ";
+		cin >> newNode -> noMhs; // Assign value to the data field of the new node
 		cout << "\nEnter the name of the student: ";
+		cin >> newNode -> name; // Assign value to the data field of the new node
+
+		// Insert the new node in the list
+		if (START == NULL || newNode->noMhs <= START->noMhs)
+		{ // step 2: insert the new node at the beginning
+
+
 	
 
 
