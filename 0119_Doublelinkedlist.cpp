3dd31973799f74db2fe3e2abef44b7109d8ac9c5
kiieeaa <kiyaadinda5@gmail.com>
2024-05-03 09:38:20 +0700

    assign value to the data

--- 0119_Doublelinkedlist.cpp
+++ 0119_Doublelinkedlist.cpp
@@ -12,6 +12,8 @@ struct Node
 	{
 		Node* newNode = new Node(); // step 1: create a new node cout << "\nEnter the roll number of the student: ";
 		cout << "\nEnter the roll number of the student:";
+		cin >> newNode->noMhs; // Assign value to the data field of the new node
+
 	}
 
 
