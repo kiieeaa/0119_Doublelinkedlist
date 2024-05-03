903740ec1084aa3368a1a9f3060e61a0bd3b2aab
kiieeaa <kiyaadinda5@gmail.com>
2024-05-03 09:34:54 +0700

    step 1

--- 0119_Doublelinkedlist.cpp
+++ 0119_Doublelinkedlist.cpp
@@ -5,10 +5,14 @@ struct Node
 	Node *next;
 	Node *prev;
 };
+
 	Node *START = NULL;
 
 	void addNode()
-	{}
+	{
+		Node* newNode = new Node(); // step 1: create a new node cout << "\nEnter the roll number of the student: ";
+
+	}
 
 
 
