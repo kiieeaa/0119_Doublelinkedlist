34c0034f125715c5bda4c6e1498d9b95016b18c2
kiieeaa <kiyaadinda5@gmail.com>
2024-05-03 10:36:26 +0700

    step 6

--- 0119_Doublelinkedlist.cpp
+++ 0119_Doublelinkedlist.cpp
@@ -32,6 +32,12 @@ void addNode()
 				START->prev = newNode; // step 4: make the first node point to the new node
 			}
 			newNode->prev = NULL; // step 5: make the new node point to NULL
+			START = newNode;        // step 6: make the new node the first node
+
+			else
+
+				// Insert the new node in the middle or at the end
+
 
 
 
