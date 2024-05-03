97427bcaaf4d2942ae6c1a6c3f7f3c2c3b7f97f8
kiieeaa <kiyaadinda5@gmail.com>
2024-05-03 09:43:32 +0700

    nenter the name of the student

--- 0119_Doublelinkedlist.cpp
+++ 0119_Doublelinkedlist.cpp
@@ -1,20 +1,20 @@
 struct Node
 {
 	int noMhs;
-	string  name;
+	string name;
 	Node *next;
 	Node *prev;
 };
 
-	Node *START = NULL;
+Node* START = NULL;
 
-	void addNode()
+void addNode()
 	{
 		Node* newNode = new Node(); // step 1: create a new node cout << "\nEnter the roll number of the student: ";
-		cout << "\nEnter the roll number of the student:";
+		coun << "\nEnter the roll number of the student:";
 		cin >> newNode->noMhs; // Assign value to the data field of the new node
-
-	}
+		cout << "\nEnter the name of the student: ";
+	
 
 
 
